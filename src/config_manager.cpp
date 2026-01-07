6c2e738c92e7f8bb88ae9c1d671ece252b2d3ef5
Jacob Warrer <JacobWarrer@gmail.com>
2026-01-07 12:08:55 +0100

    Create config_manager
    Created singleton cached config manager for reading and writing configs
    in the last 4kb of the qspi flash.
    
    The entire flash structure layout is still not complete and would need
    some more work.
    Furthermore the reading of specific part of the config need to be
    correctly addressed in the read buffer.

---
+++ src/config_manager.cpp
@@ -0,0 +1,111 @@
+#include "config_manager.h"
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <hardware/flash.h>
+#include <hardware/regs/addressmap.h>
+#include <hardware/sync.h>
+#include <pico/critical_section.h>
+#include <pico/types.h>
+
+ConfigManager::ConfigManager() {
+  critical_section_init(&this->crit_sec_);
+}
+
+ConfigManager::~ConfigManager() {
+  critical_section_deinit(&this->crit_sec_);
+}
+
+bool ConfigManager::exists() {
+  return validate_header();
+};
+
+bool ConfigManager::erase_all() {
+  uint32_t status = save_and_disable_interrupts();
+
+  flash_range_erase(config::CONFIG_OFFSET, config::SECTOR_SIZE);
+
+  restore_interrupts_from_disabled(status);
+
+  const uint8_t *flash_ptr = get_flash_ptr();
+  for (size_t i = 0; i < config::SECTOR_SIZE; ++i) {
+    if (flash_ptr[i] != 0xFF) {
+      printf("ERROR: Erase verification failed at offset %zu\n", i);
+      return false;
+    }
+  }
+  return true;
+}
+
+bool ConfigManager::wifi_load(wifi_config &cfg) {
+  critical_section_enter_blocking(&this->crit_sec_);
+  if (!wifi_cache_valid_) {
+    // read_from_flash()
+  }
+  critical_section_exit(&this->crit_sec_);
+  return true;
+}
+
+const uint8_t *ConfigManager::get_flash_ptr() {
+  const uintptr_t flash_addr =
+      static_cast<uintptr_t>(XIP_BASE) + config::CONFIG_OFFSET;
+  // This is unavoidable for memory-mapped hardware
+  // NOLINTNEXTLINE(performance-no-int-to-ptr)
+  return reinterpret_cast<const uint8_t *>(flash_addr);
+}
+
+bool ConfigManager::validate_header() {
+  const uint8_t *flash_ptr = get_flash_ptr();
+  const auto *header = reinterpret_cast<const flash_header *>(get_flash_ptr());
+
+  if (header->magic != config::MAGIC) {
+    return false;
+  }
+
+  if (header->version != config::VERSION) {
+    return false;
+  }
+
+  uint32_t stored_crc;
+  memcpy(&stored_crc, &flash_ptr[config::SECTOR_SIZE - 4], 4);
+
+  uint32_t calculated_crc32 =
+      calculate_crc32(flash_ptr, config::SECTOR_SIZE - 4);
+
+  if (stored_crc != calculated_crc32) {
+    printf("CRC mismatch! Stored=0x%08X, Calculated=0x%08X", stored_crc,
+           calculated_crc32);
+    return false;
+  }
+
+  return true;
+}
+
+uint32_t ConfigManager::calculate_crc32(const uint8_t *data, size_t len) {
+  uint32_t crc = 0xFFFFFFFF;
+
+  for (size_t i = 0; i < len; ++i) {
+    uint8_t index = (crc ^ data[i]) & 0xFF;
+    crc           = (crc >> 8) ^ config_detail::crc_table[index];
+  }
+
+  return ~crc;
+}
+
+void ConfigManager::read_config_sector(uint8_t *buffer) {
+  const uint8_t *flash_ptr = get_flash_ptr();
+  std::memcpy(buffer, flash_ptr, config::SECTOR_SIZE);
+}
+
+bool ConfigManager::write_config_sector(const uint8_t *buffer) {
+  uint32_t nvec = save_and_disable_interrupts();
+
+  flash_range_erase(config::CONFIG_OFFSET, config::SECTOR_SIZE);
+  flash_range_program(config::CONFIG_OFFSET, buffer, config::SECTOR_SIZE);
+
+  restore_interrupts(nvec);
+
+  const uint8_t *flash_ptr = get_flash_ptr();
+  return (memcmp(flash_ptr, buffer, config::SECTOR_SIZE) == 0);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
+#include <cstdint>
 #include <pico/stdio.h>
 
 #include <cstdio>
 
+#include "config_manager.h"
+
 auto main(int argc, char *argv[]) -> int {
 
-    stdio_init_all();
-    puts("Launching Lora2MQTT Gateway");
-    return 0;
+  stdio_init_all();
+  printf("Launching Lora2MQTT Gateway\n");
+
+  // test memory pointer
+  auto       &cfg       = ConfigManager::instance();
+  const auto *flash_ptr = ConfigManager::get_flash_ptr();
+  printf("Flash Config pointer: %p\n", flash_ptr);
+  printf("First 4 bytes of config space:\n %02X %02X %02X %02X\n", flash_ptr[0],
+         flash_ptr[1], flash_ptr[2], flash_ptr[3]);
+  return 0;
 }
